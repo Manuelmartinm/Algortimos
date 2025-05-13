b4ecc7b2cf6667fb24a88da065f233a0cccc81fb
Manuelmartinm <manuelmartiin.18@gmail.com>
2025-05-13 10:04:28 +0200

    Todo

---
+++ testalgoritmos.java
@@ -0,0 +1,29 @@
+package entornos;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class testalgoritmos {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void testFibonacci() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFactorial() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrimo() {
+		fail("Not yet implemented");
+	}
+
+}
